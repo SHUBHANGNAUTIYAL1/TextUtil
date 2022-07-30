ef3efe7553a49852043acf975ff8e847f686b8bb
shubhang <shubhang999454@gmail.com>
2022-07-30 13:49:03 +0530

    Updates

---
+++ precache-manifest.3c6b223acbf0f0c8b0a2ef9090bca13e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6072ebbc46a71fe11c20",
+    "url": "/TextUtil/static/css/main.56447a3d.chunk.css"
+  },
+  {
+    "revision": "6072ebbc46a71fe11c20",
+    "url": "/TextUtil/static/js/main.6072ebbc.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/TextUtil/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "504dd1c489964c95b869",
+    "url": "/TextUtil/static/js/2.504dd1c4.chunk.js"
+  },
+  {
+    "revision": "007f6372142bd9e1cb23",
+    "url": "/TextUtil/static/js/runtime~main.007f6372.js"
+  },
+  {
+    "revision": "b58489224e658e4fca1f99030bf8692d",
+    "url": "/TextUtil/index.html"
+  }
+];
